9d62889e05cce07cff882b7e20402e566dddb83b
rider-nice <Vaivit@icloud.com>
2021-03-30 11:50:14 +0700

    Exercise4 V.1

---
+++ Exercise4_Vaivit_C.py
@@ -0,0 +1,9 @@
+FoundEng=10           #ข้อมูลคะแนนรายวิชา Foundation English
+GenBus=20             #ข้อมูลคะแนนรายวิชา General Business
+IntroCom=30           #ข้อมูลคะแนนรายวิชา Introduction to Computer Systems
+ComProgram=40         #ข้อมูลคะแนนรายวิชา Computer Programming
+print("--Your Score--")
+print("Foundation English :               ",FoundEng)
+print("General Business :                 ",GenBus)
+print("Introduction to Computer Systems : ",IntroCom)
+print("Computer Programming :             ",ComProgram)
