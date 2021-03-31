269a3ba5ac18bfdc1563a4dbb0c0ef67066b7d3e
rider-nice <Vaivit@icloud.com>
2021-03-31 11:14:27 +0700

    Create Exercise5_1_Vaivit_C.py

---
+++ Exercise5_1_Vaivit_C.py
@@ -0,0 +1,6 @@
+Number1=int(input("The First Number is : "))
+Number2=int(input("The Second Number is : "))
+print(Number1,"+",Number2,"=",Number1+Number2)
+print(Number1,"-",Number2,"=",Number1-Number2)
+print(Number1,"*",Number2,"=",Number1*Number2)
+print(Number1,"/",Number2,"=",int(Number1/Number2))
